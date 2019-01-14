531d53dc37299da04776803f83e2ee860a5bab4c
Gianluca Petrillo <petrillo@slac.stanford.edu>
2019-01-14 13:43:09 -0600

    Quantities library moved to LArSoft (lardataalg).

--- icaruscode/Light/Algorithms/PMTsimulationAlg.h
+++ icaruscode/Light/Algorithms/PMTsimulationAlg.h
@@ -14,16 +14,16 @@
 
 
 // ICARUS libraries
-#include "icaruscode/Utilities/quantities/spacetime.h" // microsecond, ...
-#include "icaruscode/Utilities/quantities/frequency.h" // hertz, gigahertz
-#include "icaruscode/Utilities/quantities/electronics.h" // tick, counts_f
-#include "icaruscode/Utilities/quantities/electromagnetism.h" // picocoulomb
 
 // LArSoft libraries
-#include "lardataobj/RawData/OpDetWaveform.h"
-#include "lardataobj/Simulation/SimPhotons.h"
 #include "lardataalg/DetectorInfo/LArProperties.h"
 #include "lardataalg/DetectorInfo/DetectorClocks.h"
+#include "lardataalg/Utilities/quantities/spacetime.h" // microsecond, ...
+#include "lardataalg/Utilities/quantities/frequency.h" // hertz, gigahertz
+#include "lardataalg/Utilities/quantities/electronics.h" // tick, counts_f
+#include "lardataalg/Utilities/quantities/electromagnetism.h" // picocoulomb
+#include "lardataobj/RawData/OpDetWaveform.h"
+#include "lardataobj/Simulation/SimPhotons.h"
 
 // framework libraries
 #include "fhiclcpp/types/Atom.h"
