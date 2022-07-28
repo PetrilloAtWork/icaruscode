cac8ce430a45a27a90738541087bb2c4179c836d
Gianluca Petrillo <petrillo@slac.stanford.edu>
2022-07-28 16:27:01 -0500

    Removed framework dependencies from trigger dictionary library
    The dictionary library icaruscode_Analysis_trigger_Objects contains
    definitions for a standalone ROOT tree, and is not art-independent.
    The art associations that were in the same dictionary (for convenience)
    have been moved to the experiment object dictionary icaruscode_IcarusObj.

--- icaruscode/IcarusObj/classes.h
+++ icaruscode/IcarusObj/classes.h
@@ -5,6 +5,9 @@
 #include "icaruscode/IcarusObj/OpDetWaveformMeta.h"
 #include "sbnobj/ICARUS/PMT/Trigger/Data/OpticalTriggerGate.h"
 #include "sbnobj/ICARUS/PMT/Trigger/Data/TriggerGateData.h"
+#include "lardataobj/AnalysisBase/T0.h"
+#include "lardataobj/RecoBase/PFParticle.h"
+#include "lardataobj/Simulation/BeamGateInfo.h"
 #include "lardataobj/RawData/OpDetWaveform.h"
 #include <vector>
 
