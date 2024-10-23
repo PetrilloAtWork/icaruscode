ccc3ce8292523d403538cea1685af5505ad21611
Gianluca Petrillo <petrillo@slac.stanford.edu>
2024-10-22 19:52:40 -0500

    Implementation of PMT discrimination in 16 ns blocks
    Configurations are provided for the old code ("legacy") and for the current understanding
    about how the hardware always worked (Run4).
    A Run3 configuration placeholder is also there, which would describe the discrimination
    on trailing edge instead of leading one, but that is incomplete because this feature
    is simply not implemented yet.

--- icaruscode/PMT/Trigger/Algorithms/ManagedTriggerGateBuilder.cxx
+++ icaruscode/PMT/Trigger/Algorithms/ManagedTriggerGateBuilder.cxx
@@ -11,6 +11,9 @@
 // ICARUS libraries
 #include "icaruscode/PMT/Trigger/Algorithms/ManagedTriggerGateBuilder.h"
 
+// LArSoft libraries
+#include "larcorealg/CoreUtils/enumerate.h"
+
 // C/C++ standard libraries
 #include <algorithm> // std::max()
 
@@ -21,9 +24,24 @@
 icarus::trigger::ManagedTriggerGateBuilder::ManagedTriggerGateBuilder
   (Config const& config)
   : Base_t(config)
-  , fSamplePrescale(std::max(config.SamplePrescale(), std::size_t{ 1 }))
+  , fPatternIndices(patternToIndices(config.SamplingPattern()))
+  , fBlockSize(config.SamplingPattern().size())
+  , fBlockTimeReference(config.BlockTimeReference())
   , fSampleOffset(config.SampleOffset())
-{}
+{
+  if (fBlockSize == 0) {
+    throw cet::exception{ "ManagedTriggerGateBuilder" }
+      << "Configuration error: '" << config.SamplingPattern.name()
+      << "' has no samples.\n";
+  }
+  
+  if (fPatternIndices.empty()) {
+    throw cet::exception{ "ManagedTriggerGateBuilder" }
+      << "Configuration error: '" << config.SamplingPattern.name()
+      << "' specifies a pattern with no active sample.\n";
+  }
+  
+}
 
 
 //------------------------------------------------------------------------------
@@ -45,30 +63,55 @@ void icarus::trigger::ManagedTriggerGateBuilder::dumpLocalConfiguration(
 ) const {
   
   class IndentFactory {
-    std::string const& fIndent;
+    std::string const fNewLineIndent;
     std::string const& fFirstIndent;
     
     unsigned int fCount = 0;
     
       public:
     IndentFactory(std::string const& indent, std::string const& firstIndent)
-      : fIndent(indent), fFirstIndent(firstIndent) {}
+      : fNewLineIndent('\n' + indent), fFirstIndent(firstIndent) {}
     std::string const& operator() ()
-      { return (fCount++ == 0)? fFirstIndent: fIndent; }
+      { return (fCount++ == 0)? fFirstIndent: fNewLineIndent; }
   }; // IndentFactory
   
-  IndentFactory nextIndent{ indent, firstIndent };
-
-  if (fSamplePrescale > 1) {
-    out << nextIndent()
-      << " * use only one out of " << fSamplePrescale << " samples";
+  IndentFactory newLine{ indent, firstIndent };
+  
+  out << newLine()
+    << " * state is decided by blocks of " << fBlockSize << " samples";
+  if (fBlockSize != fPatternIndices.size()) {
+    out << newLine()
+      << " * only " << fPatternIndices.size()
+      << " samples will be tested in each block:";
+    for (std::ptrdiff_t const index: fPatternIndices)
+      out << " " << index;
   }
+  if ((fBlockSize != 1) || (fBlockTimeReference != 0)) {
+    out << newLine() << " *";
+    if (fBlockTimeReference == 0) out << " the first sample";
+    else out << " sample #" << fBlockTimeReference << " from the start";
+    out << " of each block will be the block time reference";
+  }
+  
   if (fSampleOffset > 0) {
-    out << nextIndent()
+    out << newLine()
       << " * skip the first " << fSampleOffset << " samples of each waveform";
   }
   
 } // icarus::trigger::ManagedTriggerGateBuilder::dumpLocalConfiguration()
+
+
+//------------------------------------------------------------------------------
+std::vector<std::ptrdiff_t>
+icarus::trigger::ManagedTriggerGateBuilder::patternToIndices
+  (std::vector<bool> const& pattern)
+{
+  std::vector<std::ptrdiff_t> indices;
+  for (auto const& [ index, enabled ]: util::enumerate(pattern)) {
+    if (enabled) indices.push_back(index);
+  }
+  return indices;
+} // icarus::trigger::ManagedTriggerGateBuilder::patternToIndices()
 
 
 //------------------------------------------------------------------------------
